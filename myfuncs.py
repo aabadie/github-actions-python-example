3ef7143594b87a9eef45723b4764e54b5d9e3fff
Alexandre Abadie <alexandre.abadie@inria.fr>
2022-11-25 11:33:18 +0100

    apply black reformating

--- myfuncs.py
+++ myfuncs.py
@@ -1,16 +1,20 @@
 """Some useless mathematical utility functions."""
 
+
 def add(a, b):
     """Return the sum of a and b."""
     return a + b
+
 
 def sub(a, b):
     """Substract b from a."""
     return a - b
 
+
 def multiply(a, b):
     """Multiply a by b."""
     return a * b
+
 
 def divide(a, b):
     """Divide a by b."""
@@ -19,6 +23,7 @@ def divide(a, b):
     except ZeroDivisionError:
         return None
 
+
 def power(a, b):
     """Return a power b."""
-    return a ** b
+    return a**b
--- test_myfuncs.py
+++ test_myfuncs.py
@@ -2,6 +2,7 @@ import pytest
 
 from myfuncs import add, sub, multiply, divide, power
 
+
 @pytest.mark.parametrize(
     "a,b,res",
     [
@@ -10,7 +11,7 @@ from myfuncs import add, sub, multiply, divide, power
         (42, 0, 42),
         (42, -42, 0),
         (-42, 42, 0),
-    ]
+    ],
 )
 def test_add(a, b, res):
     assert add(a, b) == res
@@ -25,7 +26,7 @@ def test_add(a, b, res):
         (42, 42, 0),
         (42, -42, 84),
         (-42, 42, -84),
-    ]
+    ],
 )
 def test_sub(a, b, res):
     assert sub(a, b) == res
@@ -40,7 +41,7 @@ def test_sub(a, b, res):
         (42, 1, 42),
         (1, 42, 42),
         (-1, 42, -42),
-    ]
+    ],
 )
 def test_multiply(a, b, res):
     assert multiply(a, b) == res
@@ -55,7 +56,7 @@ def test_multiply(a, b, res):
         (42, 1, 42),
         (1, 2, 0.5),
         (-1, 2, -0.5),
-    ]
+    ],
 )
 def test_divide(a, b, res):
     assert divide(a, b) == res
@@ -71,9 +72,7 @@ def test_divide(a, b, res):
         (2, 1, 2),
         (2, 2, 4),
         (2, -1, 0.5),
-    ]
+    ],
 )
 def test_power(a, b, res):
     assert power(a, b) == res
-
-
